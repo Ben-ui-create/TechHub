bd41f722ab7e6ca121aa7de3cd3bd2a7efbe6c91
Benik Sargsyan <beniksargsyan349@gmail.com>
2026-08-02 23:16:21 +0400

    updated app.js logic

--- app.js
+++ app.js
@@ -2,15 +2,38 @@ import 'dotenv/config';
 import morgan from 'morgan';
 import {createServer} from 'http';
 import express from 'express';
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
+import {fileURLToPath} from 'url';
+import path from 'path';
 
 import './migrate.js';
 
+import errorHandler from './middlewares/errorHandler.js';
+import sessionMiddleware from "./config/session.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+//import routes
+
 const app = express();
 
-const {PORT} = process.env;
+const {PORT, COOKIE_SECRET} = process.env;
+
+app.set('views', path.resolve('views'));
+app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(sessionMiddleware);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser(COOKIE_SECRET));
+
+//app.use(routes);
+
+app.use(errorHandler.notFound);
+app.use(errorHandler.errors);
 
 const server = createServer(app);
 
